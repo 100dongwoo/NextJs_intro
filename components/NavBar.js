c83a3ed8bf0af1d36ea1ba3a2593d3f50956a47d
100dongwoo <qorehddn123@naver.com>
2022-01-29 22:27:45 +0900

    styled jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,30 +1,29 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './NavBar.module.css';
 const NavBar = () => {
     const router = useRouter();
 
     return (
         <nav>
             <Link href={'/'}>
-                <a
-                    className={`${styles.link} ${
-                        router.pathname === '/' ? styles.active : ''
-                    }`}
-                >
-                    home
-                </a>
+                <a className={router.pathname === '/' ? 'active' : ''}>home</a>
             </Link>
             <Link href={'/about'}>
-                <a
-                    className={[
-                        styles.link,
-                        router.pathname === '/about' ? styles.active : '',
-                    ].join(' ')}
-                >
+                <a className={router.pathname === '/about' ? 'active' : ''}>
                     about
                 </a>
             </Link>
+            <style jsx>{`
+                nav {
+                    background-color: tomato;
+                }
+                a {
+                    text-decoration: none;
+                }
+                .active {
+                    color: red;
+                }
+            `}</style>
         </nav>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import NavBar from "../components/NavBar";
+import NavBar from '../components/NavBar';
 
 export default function Home() {
-
     return (
         <div>
-            <NavBar/>
-            <h1>Hello</h1>
+            <NavBar />
+            <style jsx>{`
+                nav {
+                    background-color: tomato;
+                }
+                a {
+                    text-decoration: blue;
+                }
+            `}</style>
+            <h1 className={'active'}>Hello</h1>
         </div>
-
     );
 }

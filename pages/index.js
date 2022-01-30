41356b24b693a89e2b03cc4be585a470110bc0a3
100dongwoo <qorehddn123@naver.com>
2022-01-30 15:58:52 +0900

    add moive detail

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,51 @@
 import Seo from '../components/Seo';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 export default function Home({ results }) {
+    const router = useRouter();
+    const onClick = (id, title) => {
+        router.push(
+            {
+                pathname: `/movies/${id}`,
+                query: {
+                    title: title,
+                },
+            },
+            `/movies/${id}`
+        );
+    };
     return (
         <div className='container'>
             <Seo title='Home' />
 
             {results?.map((movie) => (
-                <div className='movie' key={movie.id}>
+                <div
+                    className='movie'
+                    key={movie.id}
+                    onClick={() => {
+                        onClick(movie.id, movie.original_title);
+                    }}
+                >
                     <img
-                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        alt='image'
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                     />
-                    <h4>{movie.original_title}</h4>
+                    <Link
+                        href={
+                            ({
+                                pathname: `/movies/${movie.id}`,
+                                query: {
+                                    title: movie.original_title,
+                                },
+                            },
+                            `/movies/${movie.id}`)
+                        }
+                    >
+                        <a>
+                            <h4>{movie.original_title}</h4>
+                        </a>
+                    </Link>
                 </div>
             ))}
             <style jsx>{`
@@ -19,6 +54,10 @@ export default function Home({ results }) {
                     grid-template-columns: 1fr 1fr;
                     padding: 20px;
                     gap: 20px;
+                }
+
+                .movie {
+                    cursor: pointer;
                 }
                 .movie img {
                     max-width: 100%;
